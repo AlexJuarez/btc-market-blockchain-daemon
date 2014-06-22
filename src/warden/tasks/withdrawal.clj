8765ed40871e054ce8136549f9cc30dc3641233c
Alexander Juarez <couatli@gmail.com>
2014-06-22 03:50:39 -0700

    fixed a typo and finished the withdrawal script

--- src/warden/tasks/withdrawal.clj
+++ src/warden/tasks/withdrawal.clj
@@ -16,17 +16,17 @@
   (let [amt (:amt (first (select audits (fields :user_id) (aggregate (sum :amount) :amt) (group :user_id) (where {:user_id user_id}))))]
     (if (>= amt 0)
       (do
-        (btc/sendtoaddress :bitcoinaddress address :amount amount)
-        (update withdrawal (set-fields {:processed true}) (where {:id id})))
-      (update withdrawal (set-fields {:locked true}) (where {:id id})))))
+        (btc/sendtoaddress :bitcoinaddress address :amount amount :config config)
+        (update withdrawals (set-fields {:processed true}) (where {:id id})))
+      (update withdrawals (set-fields {:locked true}) (where {:id id})))))
 
 (defn start-task []
-  (let [w (select withdrawal (order :created_on :desc))]
-    (when (> (count w) 0)
-      (loop [withdrawals w
+  (let [ws (select withdrawals (where {:processed false :locked false}) (order :created_on :desc))]
+    (when (> (count ws) 0)
+      (loop [ws ws
              balance (btc/getbalance :config config)]
-        (let [w (first withdrawals)]
-          (when-not (or (<= (count withdrawals) 0)
+        (let [w (first ws)]
+          (when-not (or (<= (count ws) 0)
                      (<= (- balance (:amount w)) 0))
             (process w)
-            (recur (rest withdrawals) (- balance (:amount w)))))))))
+            (recur (rest ws) (- balance (:amount w)))))))))
