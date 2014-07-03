ef5e6e25fee57479041bc736fab07568f58a8395
Alexander Juarez <couatli@gmail.com>
2014-07-02 21:52:09 -0700

    deposit now updates cache :noir :user :btc

--- src/warden/tasks/deposit.clj
+++ src/warden/tasks/deposit.clj
@@ -6,6 +6,7 @@
    [environ.core :only [env]])
   (:require
    [clj-btc.core :as btc]
+   [warden.cache :as cache]
    [warden.util :as util]))
 
 (def config {:rpcpassword "whitecity"
@@ -15,9 +16,16 @@
 
 (defn handle-details [{account "account" category "category" amount "amount"} txid]
   (if (= category "receive")
-      (let [user-id (util/parse-int account)]
-          (insert audits (values {:amount amount :tx txid  :role "deposit" :user_id user-id}))
-          (update users (set-fields {:btc (raw (str "btc + " amount))}) (where {:id user-id})))))
+      (let [user-id (util/parse-int account)
+            user (update users (set-fields {:btc (raw (str "btc + " amount))}) (where {:id user-id}))]
+        (do
+          (when-not (and (nil? user) (nil? (:session user)))
+            (let [session (.toString (:session user))
+                  data (cache/get session)]
+              (cache/set session (assoc-in data [:noir :user :btc] (:btc user)))
+              (println (:btc user))))
+          (insert audits (values {:amount amount :tx txid  :role "deposit" :user_id user-id})))
+        )))
 
 (defn process [tx]
   (let [info (btc/gettransaction :txid tx :config config)
